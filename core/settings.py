644e7a872501d3035e7bda71baccfdbcb219757c
ali <alijoghataee77@gmail.com>
2025-02-21 09:31:56 +0330

    feat(accounts): create accounts app (maybe authentication system needed later)

--- core/settings.py
+++ core/settings.py
@@ -60,7 +60,7 @@ BASE_APPS = [
 ]
 
 PROJECT_APPS = [
-
+    'accounts',
 ]
 
 THIRD_PARTY_APPS = [
@@ -155,3 +155,5 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
